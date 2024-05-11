fe98fb4e4b61954d0d5ecf88e779ab59f60a8b2b
Khanh Huynh <hongkhanh2901@gmail.com>
2024-05-12 06:16:25 +0700

    update project

---
+++ app/src/main/java/vn/iostar/uber/activities/client/ChooseTypePayment.java
@@ -0,0 +1,17 @@
+package vn.iostar.uber.activities.client;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import vn.iostar.uber.R;
+
+public class ChooseTypePayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_type_payment);
+    }
+}
